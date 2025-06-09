f96282bf8dbb7d7ab81b921853d00ffc99910398
José Augusto <augustoreal.santos@gmail.com>
2025-06-09 16:15:58 -0300

    Ex18

---
+++ ListasC++/Lista-1/Ex18/Ex18.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+
+// 18. Faça um programa que receba uma temperatura em Celsius, calcule e mostre essa temperatura em Fahrenheit. Sabe-se que: F = (C × 9/5) + 32
+
+int main()
+{
+    float celcius{}, fahrenheit{};
+
+    cout << "Qual eh a temperatura em celcius(C)? ";
+    cin >> celcius;
+
+    fahrenheit = (celcius * 9/5) + 32;
+
+    cout << "A temperatura em fahrenheit eh: " << fahrenheit << " F";
+
+    return 0;
+}
+
+// Completed
