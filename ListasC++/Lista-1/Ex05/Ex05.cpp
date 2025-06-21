fff8e1b3c6e057e3442f1b9f91b35435ddb9579c
José Augusto <augustoreal.santos@gmail.com>
2025-06-21 09:56:37 -0300

    Ajuste de alguns exercicios

--- ListasC++/Lista-1/Ex05/Ex05.cpp
+++ ListasC++/Lista-1/Ex05/Ex05.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
+#include <iomanip>
 
 using std::cout;
 using std::cin;
+using std::setprecision;
 
 // 5. Faça um programa que receba o preço de um produto, calcule e mostre o novo preço, sabendo-se que este sofreu um desconto de 10%.
 
 int main()
 {
-	double preco = 0;
+	double price{};
 
 	cout << "Insira o preco do produto: ";
-	cin >> preco;
+	cin >> price;
 
-	double novo_preco = preco - (preco * 0.10);
+	double new_price{ price - (price * 0.10) };
 
-	cout << "O preco do produto apos o desconto de 10% eh igual a: " << novo_preco << " R$";
+	cout << "O preco do produto apos o desconto de 10% eh igual a: " << setprecision(4) << new_price << " R$";
 
 	return 0;
 }
