bb2d63d98080b47534314ea2d4b0807b9c883c90
José Augusto <augustoreal.santos@gmail.com>
2025-07-29 10:34:00 -0300

    Ch02 Incompleto

---
+++ TonyGaddis/IntroductionC++/Ch02/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+/*
+    2. The Golden Ratio
+        The golden ratio is a ratio that appears in nature and produces pleasing, harmonious
+        proportions. For rectangles, if the width is approximately 1.62 times the height, they
+        will be in this ratio. Write a program that calculates the appropriate width for a rectangle
+        with height 5 cm so that it fulfills this ratio.
+*/
+
+int main()
+{
+    return 0;
+}
