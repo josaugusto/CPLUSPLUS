aeffbdbadeab75cc77bf72deb65b3d9e4c2bb901
José Augusto <augustoreal.santos@gmail.com>
2025-08-05 08:30:13 -0300

    Ch05 Completed

---
+++ TonyGaddis/IntroductionC++/Ch05/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <iomanip>
+
+/*
+    5. Pounds to Kilograms
+        The SI system of units is an internationally agreed decimal system of measurement that
+        uses the kilogram as the unit of weight. Earlier systems used different units, such as
+        the stone. In 1835, the United Kingdom defined the stone as weighing 14 pounds, or
+        6.35 kg. Write a program that stores your personal weight in pounds in a variable and
+        calculates and displays your weight in both stones and kilograms.
+*/
+
+int main()
+{
+    constexpr double weightPounds{166.11831}; // 75.35 kg
+    constexpr double weightStones{weightPounds / 14};
+    constexpr double weightKilograms{weightStones * 6.35};
+
+    std::cout << std::fixed << std::setprecision(2);
+    std::cout << "Weight Stones: " << weightStones << " stones\n";
+    std::cout << "Weight Kilograms: " << weightKilograms << " kilograms\n";
+
+    return 0;
+}
+
+// Completed
