3c0d903609049b4d016e6dc968f9e604932b267b
José Augusto <augustoreal.santos@gmail.com>
2025-09-02 13:38:41 -0300

    Ex10 COMPLETED

---
+++ ListasC++/Lista-2/Ex10/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <iomanip>
+
+/*
+    10. O custo ao consumidor de um carro novo é a soma do custo de fábrica com a porcentagem do distribuidor e com os impostos, ambos aplicados ao custo de fábrica. Sabe-se que as porcentagens são as mesmas que estão na tabela a seguir. Faça um programa que receba o custo de fábrica de um carro e mostre o custo ao consumidor.
+
+        | CUSTO DE FÁBRICA                    | % DO DISTRIBUIDOR | % DOS IMPOSTOS |
+        | ----------------------------------- | ----------------- | -------------- |
+        | Até R$ 12.000,00                    | 5                 | isento         |
+        | Entre R$ 12.000,00 e R$ 25.000,00   | 10                | 15             |
+        | Acima de R$ 25.000,00               | 15                | 20             |
+*/
+
+int main()
+{
+    double custoFabrica{};
+
+    std::cout << "Qual o custo de fabrica do carro(R$)? ";
+    std::cin >> custoFabrica;
+
+    double custoConsumidor{};
+
+    if (custoFabrica <= 12000.00) 
+    custoConsumidor = custoFabrica + (custoFabrica * 0.05);
+    else if(custoFabrica <= 25000.00)
+        custoConsumidor = custoFabrica + (custoFabrica * 0.10) + (custoFabrica * 0.15);
+    else
+        custoConsumidor = custoFabrica + (custoFabrica * 0.15) + (custoFabrica * 0.20);
+
+    std::cout << std::fixed << std::showpoint << std::setprecision(2);
+    std::cout << "Custo ao consumidor = R$ " << custoConsumidor << '\n'; 
+
+    return 0;
+}
+
+// Completed
