8939d6400edf942947e4219ddc6b09ddf345cc0b
José Augusto <augustoreal.santos@gmail.com>
2025-09-02 09:02:52 -0300

    Ex07 Update

--- ListasC++/Lista-2/Ex07/main.cpp
+++ ListasC++/Lista-2/Ex07/main.cpp
@@ -12,7 +12,7 @@ int main()
     std::cout << "Salario do Funcionario: ";
     std::cin >> salario;
 
-    std::cout << std::fixed << std::setprecision(2);
+    std::cout << std::fixed << std::showpoint << std::setprecision(2);
     if (salario < 500.00)
     {
         float salarioReajustado{ salario + (salario * 0.30f) };
